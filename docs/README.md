e4f609d101f1e707b3881576a1eadb46132a1267
toughchb <ggang2000@gmail.com>
2022-04-14 22:52:54 +0900

    feat(SystemView): 메세지를 이용 하여 view 구현
    - 숫자 입력 메세지 출력 -> SystemView::requestBallNumberInput()
    - retry 메세지 출력 -> SystemView::requestRetryOption()
    - 스코어 메세지 출력 -> SystemView::printScore()
    - 게임 끝 메세지 출력 -> SystemView::printGameFinish()

--- docs/README.md
+++ docs/README.md
@@ -1,8 +1,13 @@
-메세지 처리
-[v] 리소스 번들을 이용해서 메세지 정의하기 
+##메세지 처리
+* 리소스 번들을 이용해서 메세지 정의하기 [v]
 
-메세지를 이용 하여 view 처리
-[] 숫자 입력 메세지 출력 -> SystemView::requestBallNumberInput()
-[] retry 메세지 출력 -> SystemView::requestRetryOption()
-[] 스코어 메세지 출력 -> SystemView::printScore()
-[] 게임 끝 메세지 출력 -> SystemView::printGameFinish()
+##메세지를 이용 하여 view 구현
+* 숫자 입력 메세지 출력 [v]
+  - SystemView::requestBallNumberInput()
+* retry 메세지 출력 [v]
+  - SystemView::requestRetryOption()
+* 스코어 메세지 출력 [v]
+  - SystemView::printScore()
+* 게임 끝 메세지 출력 [v]
+  - SystemView::printGameFinish()
+
---
+++ src/main/java/baseball/view/SystemView.java
@@ -0,0 +1,40 @@
+package baseball.view;
+
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+
+public class SystemView {
+
+    static ResourceBundle resourceBundle = ResourceBundle.getBundle("message");
+
+     public static void requestBallNumberInput() {
+         System.out.println(resourceBundle.getString("input.number"));
+     }
+
+     public static void requestRetryOption() {
+         System.out.println(resourceBundle.getString("input.retry"));
+     }
+
+    public static void printScore(int ballCount, int strikeCount) {
+        if (ballCount == 0 && strikeCount == 0) {
+            System.out.println(resourceBundle.getString("score.nothing"));
+        }
+        if (ballCount == 0 && strikeCount != 0) {
+            String string = resourceBundle.getString("score.strike");
+            System.out.println(MessageFormat.format(string, strikeCount));
+        }
+        if (ballCount != 0 && strikeCount == 0) {
+            String string = resourceBundle.getString("score.ball");
+            System.out.println(MessageFormat.format(string, ballCount));
+        }
+        if (ballCount != 0 && strikeCount != 0) {
+            String string = resourceBundle.getString("score.ball.strike");
+            System.out.println(MessageFormat.format(string, ballCount, strikeCount));
+        }
+    }
+
+    public static void printGameFinish() {
+        System.out.println(resourceBundle.getString("end"));
+    }
+
+}
