f0281c3e0f1d395165dad24c7ea9650165466b7b
toughchb <ggang2000@gmail.com>
2022-04-15 11:39:04 +0900

    feat(InputNumberValidator): 숫자 검증 기능
    - 입력 받은 숫자가 3개 인지 확인
    - 입력 받은 숫자가 1~9의 숫자인지 확인
    - 입력 받은 숫자에 중복이 없는지 확인

---
+++ src/main/java/baseball/util/InputNumberValidator.java
@@ -0,0 +1,41 @@
+package baseball.util;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+public class InputNumberValidator {
+    private static final int NUM_COUNT = 3;
+    private static final int MAX_NUM = 9;
+    private static final int MIN_NUM = 1;
+
+    public static void validate(List<Integer> numbers) {
+        countValidate(numbers);
+        numbersValidate(numbers);
+        dupNumberValidate(numbers);
+    }
+
+    private static void countValidate(List<Integer> numbers) {
+        if (numbers.size() != NUM_COUNT) throw new IllegalArgumentException(MAX_NUM + "개의 숫자를 입력 하셔야 합니다.");
+    }
+
+    private static void numbersValidate(List<Integer> numbers) {
+        Iterator<Integer> iterator = numbers.iterator();
+        while (iterator.hasNext()) {
+            numberValidate(iterator.next());
+        }
+    }
+
+    private static void numberValidate(int number) {
+        if (number < MIN_NUM || number > MAX_NUM) {
+            throw new IllegalArgumentException("잘못된 값을 입력 하셨습니다.");
+        }
+    }
+
+    private static void dupNumberValidate(List<Integer> numbers) {
+        Set<Integer> numberSet = new HashSet<>(numbers);
+        if (numbers.size() != numberSet.size()) throw new IllegalArgumentException("중복된 값을 입력 하셨습니다.");
+
+    }
+}
