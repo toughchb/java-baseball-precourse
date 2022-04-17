af30c00b3cc8358ce6d9be676c658cdd705e0137
toughchb <ggang2000@gmail.com>
2022-04-17 21:20:09 +0900

    refactor(InputNumberValidator): while loop -> for 변환

--- src/main/java/baseball/util/InputNumberValidator.java
+++ src/main/java/baseball/util/InputNumberValidator.java
@@ -3,7 +3,6 @@ package baseball.util;
 import baseball.common.BaseballGameConstants;
 
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -16,13 +15,13 @@ public class InputNumberValidator {
     }
 
     private static void countValidate(List<Integer> numbers) {
-        if (numbers.size() != BaseballGameConstants.SIZE) throw new IllegalArgumentException(BaseballGameConstants.SIZE + "개의 숫자를 입력 하셔야 합니다.");
+        if (numbers.size() != BaseballGameConstants.SIZE)
+            throw new IllegalArgumentException(BaseballGameConstants.SIZE + "개의 숫자를 입력 하셔야 합니다.");
     }
 
     private static void numbersValidate(List<Integer> numbers) {
-        Iterator<Integer> iterator = numbers.iterator();
-        while (iterator.hasNext()) {
-            numberValidate(iterator.next());
+        for (Integer number : numbers) {
+            numberValidate(number);
         }
     }
 
