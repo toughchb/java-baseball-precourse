9f42cd1904be58850211df22ad669778af030aa5
toughchb <ggang2000@gmail.com>
2022-04-17 14:26:25 +0900

    refactor(BallTest): BallTest 리팩토링

--- src/test/java/baseball/domain/BallTest.java
+++ src/test/java/baseball/domain/BallTest.java
@@ -1,30 +1,34 @@
 package baseball.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BallTest {
 
+    private Ball computerBall;
+
+    @BeforeEach
+    void init() {
+        computerBall = new Ball(1, 4);
+    }
 
     @Test
     void strike() {
-        Ball com = new Ball(1, 4);
-        BallStatus state = com.play(new Ball(1,4));
+        BallStatus state = computerBall.play(new Ball(1,4));
         assertThat(state).isEqualTo(BallStatus.STRIKE);
     }
 
     @Test
     void ball() {
-        Ball com = new Ball(1, 4);
-        BallStatus state = com.play(new Ball(2,4));
+        BallStatus state = computerBall.play(new Ball(2,4));
         assertThat(state).isEqualTo(BallStatus.BALL);
     }
 
     @Test
     void nothing() {
-        Ball com = new Ball(1, 4);
-        BallStatus state = com.play(new Ball(2,5));
+        BallStatus state = computerBall.play(new Ball(2,5));
         assertThat(state).isEqualTo(BallStatus.NOTHING);
     }
 }
