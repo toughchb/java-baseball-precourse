4e190637c7cccb7cb141cc3361c12302e788148c
toughchb <ggang2000@gmail.com>
2022-04-17 14:26:45 +0900

    refactor(BaseballGameTest): BaseballGameTest 리팩토링

--- src/test/java/baseball/domain/BaseballGameTest.java
+++ src/test/java/baseball/domain/BaseballGameTest.java
@@ -1,5 +1,6 @@
 package baseball.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -11,30 +12,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class BaseballGameTest {
 
+    private BaseballGame answers;
+
+    @BeforeEach
+    void init() {
+        answers = new BaseballGame(Arrays.asList(1, 2, 3));
+    }
+
     @Test
     void strike() {
-        BaseballGame answers = new BaseballGame(Arrays.asList(1, 2, 3));
         BallStatus status = answers.play(new Ball(1, 1));
         assertThat(status).isEqualTo(STRIKE);
     }
 
     @Test
     void ball() {
-        BaseballGame answers = new BaseballGame(Arrays.asList(1, 2, 3));
         BallStatus status = answers.play(new Ball(1, 2));
         assertThat(status).isEqualTo(BALL);
     }
 
     @Test
     void nothing() {
-        BaseballGame answers = new BaseballGame(Arrays.asList(1, 2, 3));
         BallStatus status = answers.play(new Ball(1, 4));
         assertThat(status).isEqualTo(NOTHING);
     }
 
     @Test
     void ball1_strike1() {
-        BaseballGame answers = new BaseballGame(Arrays.asList(1, 2, 3));
         GameResult result = answers.play(Arrays.asList(1, 4, 2));
         assertThat(result.getBall()).isEqualTo(1);
         assertThat(result.getStrike()).isEqualTo(1);
@@ -42,7 +46,6 @@ public class BaseballGameTest {
 
     @Test
     void strike3() {
-        BaseballGame answers = new BaseballGame(Arrays.asList(1, 2, 3));
         GameResult result = answers.play(Arrays.asList(1, 2, 3));
         assertThat(result.getBall()).isEqualTo(0);
         assertThat(result.getStrike()).isEqualTo(3);
@@ -50,7 +53,6 @@ public class BaseballGameTest {
 
     @Test
     void strike2() {
-        BaseballGame answers = new BaseballGame(Arrays.asList(1, 2, 3));
         GameResult result = answers.play(Arrays.asList(1, 2, 4));
         assertThat(result.getBall()).isEqualTo(0);
         assertThat(result.getStrike()).isEqualTo(2);
@@ -58,7 +60,6 @@ public class BaseballGameTest {
 
     @Test
     void strike1() {
-        BaseballGame answers = new BaseballGame(Arrays.asList(1, 2, 3));
         GameResult result = answers.play(Arrays.asList(1, 4, 5));
         assertThat(result.getBall()).isEqualTo(0);
         assertThat(result.getStrike()).isEqualTo(1);
